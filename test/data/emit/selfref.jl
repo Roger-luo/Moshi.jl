8e356e5445cbf296cb452e60a05a7b232f61252b
Roger-luo <rogerluo.rl18@gmail.com>
2024-07-10 13:39:59 +0200

    add a small test for selfref

--- test/data/emit/selfref.jl
+++ test/data/emit/selfref.jl
@@ -1,16 +1,17 @@
 using Test
-using Moshi.Data: @data, TypeDef, guess_self_as_any
+using Moshi.Data: @data, TypeDef, guess_self_as_any, variant_getfield
 using Moshi.Derive: @derive
 
 @data Arith begin
     Num(Int)
     Add(Arith, Arith.Type)
-    Sub(Arith, Arith) 
+    Sub(Arith, Arith)
 end
 
 @testset "selfref" begin
     x = Arith.Add(Arith.Num(1), Arith.Num(1))
-    @test Base.return_types(getproperty, Tuple{Arith.Type, Int})[1] == Union{Int, Arith.Type}
+    @test Base.return_types(getproperty, Tuple{Arith.Type,Int})[1] == Union{Int,Arith.Type}
+    @test variant_getfield(x, Arith.Add, 1) == Arith.Num(1)
 end # selfref
 
 @data SelfRef{T} begin
@@ -22,7 +23,8 @@ end
 
 @testset "selfref{T}" begin
     x = SelfRef.Ref(SelfRef.Val(1))
-    @test Base.return_types(getproperty, Tuple{SelfRef.Type{Int}, Int})[1] == Union{Int, SelfRef.Type{Int}}
+    @test Base.return_types(getproperty, Tuple{SelfRef.Type{Int},Int})[1] ==
+        Union{Int,SelfRef.Type{Int}}
 
     x = SelfRef.Ref(SelfRef.Val(1))
     y = SelfRef.Ref(SelfRef.Val(1))
