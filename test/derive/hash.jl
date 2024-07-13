136e0b3a48fd8c566eb7bb9a904de3493676269b
Roger-luo <rogerluo.rl18@gmail.com>
2024-07-14 00:24:21 +0200

    test cache eq

--- test/derive/hash.jl
+++ test/derive/hash.jl
@@ -16,10 +16,14 @@ end
 
 Data.variant_fieldtypes(Message.Move)
 Data.variant_fieldnames(Message.Move)
-@derive Message[Hash]
+@derive Message[Hash, Eq]
 
 type_hash = hash(hash(Message.Type))
 @test hash(Message.Quit()) == hash(type_hash, hash(Message.Quit))
 x = Message.Move(x=1, y=2)
 @test hash(x) == hash(2, hash(1, hash(type_hash, hash(Message.Move))))
 @test x.cache[] == hash(x)
+y = Message.Move(x=1, y=2)
+@test x == y
+@test hash(y) == hash(x)
+@test x == y
