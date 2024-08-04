7a6834b98ea626cafd64e993123f937ac9f54ded
Roger-luo <rogerluo.rl18@gmail.com>
2024-08-04 17:29:33 -0400

    add benchmark plots

--- benchmark/main.jl
+++ benchmark/main.jl
@@ -1,3 +1,4 @@
+using DataFrames
 using CairoMakie
 using BenchmarkTools
 
@@ -6,7 +7,12 @@ function benchmark(mod::Module, N::Int)
     println("$(mod): $(Base.format_bytes(Base.summarysize(xs)))")
     result = @benchmark $(getproperty(mod, Symbol("main!")))($xs)
     display(result)
-    return Base.summarysize(xs), minimum(result.times)/1e6 # ms
+    return Base.summarysize(xs), minimum(result.times) / 1e6 # ms
+end
+
+function explore_size(mod::Module)
+    bench_size = (10, 100, 1000, 10000)
+    return [benchmark(mod, n) for n in bench_size]
 end
 
 include("base.jl")
@@ -18,20 +24,76 @@ include("moshi_hacky.jl")
 include("sumtypes.jl")
 
 data = Dict(
-    "baseline" => benchmark(BaseBench, 10000),
-    "DynamicSumTypes" => benchmark(DynamicSumTypesBench, 10000),
-    "Expronicon" => benchmark(ExproniconBench, 10000),
-    "Moshi (reflection)" => benchmark(MoshiBench, 10000),
-    "Moshi (match)" => benchmark(MoshiMatchBench, 10000),
-    "Moshi (hacky)" => benchmark(MoshiHackyBench, 10000),
-    "SumType" => benchmark(SumTypeTest, 10000),
+    "baseline" => explore_size(BaseBench),
+    "DynamicSumTypes" => explore_size(DynamicSumTypesBench),
+    "Expronicon" => explore_size(ExproniconBench),
+    "Moshi (reflection)" => explore_size(MoshiBench),
+    "Moshi (match)" => explore_size(MoshiMatchBench),
+    "Moshi (hacky)" => explore_size(MoshiHackyBench),
+    "SumType" => explore_size(SumTypeTest),
 )
 
-fig = barplot(
-    [i for i in 1:length(data)],
-    [each[2] for each in values(data)],
-    axis = (
-        xticks = (1:length(data), [key for key in keys(data)]),
-        xticklabelrotation = pi/4,
-    ),
-)
+alloc = Dict{String,Vector{Float64}}()
+for (key, col) in data
+    alloc[key] = map(col) do each
+        each[1]
+    end
+end
+for (key, value) in alloc
+    key == "baseline" && continue
+    alloc[key] = round.(value ./ alloc["baseline"], digits=2)
+end
+delete!(alloc, "baseline")
+
+speed = Dict{String,Vector{Float64}}()
+for (key, col) in data
+    @show key
+    speed[key] = map(col) do each
+        each[2]
+    end
+end
+for (key, value) in speed
+    key == "baseline" && continue
+    speed[key] = round.(value ./ speed["baseline"], digits=2)
+end
+delete!(speed, "baseline")
+
+speed
+set_theme!(theme_dark())
+colors = Makie.wong_colors()
+fig = Figure(size=(800, 1000), backgroundcolor=:transparent)
+for (idx, size) in enumerate([10, 100, 1000, 10000])
+    packages = sort(collect(keys(speed)))
+    tbl = (
+        cat=[i for i in 1:length(speed) for j in 1:2],
+        height=[j == 1 ? alloc[key][idx] : speed[key][idx] for key in packages for j in 1:2],
+        grp=[j for i in 1:length(speed) for j in 1:2],
+    )
+
+    barplot(
+        fig[idx, 1],
+        tbl.cat,
+        tbl.height;
+        dodge=tbl.grp,
+        color=colors[tbl.grp],
+        axis= idx == 4 ? (
+            yticks=0:10,
+            xticks=(1:length(speed), packages),
+            xticklabelrotation = pi/4,
+            title="# of elements: $(size)",
+            xlabel="packages",
+        ) : (
+            yticks=0:10,
+            xticksvisible=false,
+            xticklabelsvisible=false,
+            title="# of elements: $(size)",
+        ),
+    )
+end
+
+Label(fig[:, 0], "relative to baseline", rotation = pi/2)
+
+elements = [PolyElement(polycolor = colors[1]), PolyElement(polycolor = colors[2])]
+Legend(fig[1, 2], elements, ["allocation", "speedup"], "Benchmark")
+
+save("benchmark.png", fig)
